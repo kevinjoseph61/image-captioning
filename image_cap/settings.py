e1e030fe0eda219645622b2bf6b2f708ba239064
kevinjoseph <kevinjoseph@commvault.com>
2021-06-05 13:11:27 +0530

    Whitenoise!

--- image_cap/settings.py
+++ image_cap/settings.py
@@ -34,6 +34,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
